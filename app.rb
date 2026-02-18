e8eb9d6c5e934dc220f18db7a6b20b97ed5944ac
LukasWilhelmsson <Lukas.wilhelmsson@elev.ga.ntig.se>
2026-02-18 16:15:36 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,30 @@
-require 'sinatra'
-require 'slim'
-require 'sqlite3'
-require 'sinatra/reloader'
-require 'bcrypt'
+require "sinatra"
+require "sqlite3"
+require "slim"
+require "sinatra/reloader"
+
+def db
+  SQLite3::Database.new("db/pokedex.db", results_as_hash: true)
+end
+
+get("/") do
+  redirect("/pokedex")
+end
+
+get("/pokedex") do
+
+  query = params["q"]
+
+  if query && !query.empty?
+    @pokemons = db.execute(
+      "SELECT * FROM pokemons WHERE name LIKE ?",
+      ["%#{query}%"]
+    )
+  else
+    @pokemons = db.execute("SELECT * FROM pokemons")
+  end
+
+  @pokemons ||= []
+
+  slim :"pokedex/index"
+end
